f78bb98070cd680a515b2bffb2c2206e7e5a49ed
unknown <tung345bb@gmail.com>
2024-12-16 20:49:28 +0700

    first commit

---
+++ ttt-lesson10/database/migrations/2024_12_16_134349_create_vattu_table.php
@@ -0,0 +1,28 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+return new class extends Migration
+{
+    /**
+    * Run the migrations.
+    */
+    public function up(): void
+    {
+    Schema::create('vattu', function (Blueprint $table) {
+    // $table->id(); // mặc định id là khóa chính
+    $table->string('MaVTu')->primary();
+    $table->string('TenVTu')->unique();
+    $table->string('DvTinh');
+    $table->float('PhanTram');
+    // $table->timestamps(); tạo trường mặc định
+    });
+    }
+    /**
+    * Reverse the migrations.
+    */
+    public function down(): void
+    {
+    Schema::dropIfExists('vattu');
+    }
+    };
